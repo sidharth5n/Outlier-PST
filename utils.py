c4a42ba3edf59eb8c0649e1f3aa91255a812bded
sidharth5n <sidhunair5@gmail.com>
2020-09-26 11:46:37 +0530

    Added preprocessing

--- utils.py
+++ utils.py
@@ -4,16 +4,18 @@ os.environ['PATH'] = os.environ['PATH'] + ';' + os.environ['CONDA_PREFIX'] + r"\
 import pydotplus
 from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.cluster.vq import kmeans, whiten
 
 def show(tree):
       """Creates a DOT file of the tree and displays the tree
-      
+
       Parameters
       ----------
       tree : PST
              Probabilistic suffix tree object
       """
-      
+
       f = open("PST.dot", 'w')
       f.write("graph PST {\n")
       f.write("node0" + "[label = Root];\n")
@@ -39,3 +41,32 @@ def show(tree):
       img = Image.open("PST.png")
       plt.imshow(img)
       plt.axis("off")
+
+def load_and_preprocess(file, directory, k):
+    """
+    Loads the data, performs K-Means clustering on the data and assigns label
+    to each data
+
+    Parameters
+    ----------
+    file : str
+           Name of the file containing data
+    directory : str
+                Directory in which dataset is present
+    k : int
+        No. of clusters
+
+    Returns
+    -------
+    labels : list
+             Label of each data point
+    """
+    data = np.genfromtxt(os.path.join(directory, file), delimiter = ',', skip_header = 1)
+    data = np.array(data[:,1], ndmin = 2).T
+    data = whiten(data)
+    means, distortion = kmeans(data, k_or_guess = 5)
+    labels = []
+    label = [chr(x) for x in range(ord('A'), ord('A') + k)]
+    for i in range(len(data)):
+        labels.append(label[np.argmin((means - data[i])**2)])
+    return labels
